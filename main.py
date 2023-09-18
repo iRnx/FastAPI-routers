7202796354639d723905f13e7bf2e431b897198b
Renan <renan.rg@hotmail.com.br>
2023-09-18 15:31:56 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from routes.curso_router import router as curso_router
+from routes.usuario_router import router as usuario_router
+
+app = FastAPI()
+app.include_router(curso_router, tags=['cursos'])
+app.include_router(usuario_router, tags=['usuarios'])
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
